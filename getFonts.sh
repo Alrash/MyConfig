fb058fa8b8bc0cfeadd493dd1b0598fb280acf0a
bwfullcolor <kasukuikawai@gmail.com>
2016-07-03 14:58:48 +0800

    modfiy some mistakes

--- getFonts.sh
+++ getFonts.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-rm -rf awesome-terminal-fonts && git clone -b patching-strategy git@github.com:gabrielelana/awesome-terminal-fonts.git && cp awesome-terminal-fonts/patched/* ~/.fonts && sudo fc-cache -rv && rm -rf awesome-terminal-terminal
+rm -rf awesome-terminal-fonts
+git clone -b patching-strategy git@github.com:gabrielelana/awesome-terminal-fonts.git && cp awesome-terminal-fonts/patched/* ~/.fonts && sudo fc-cache -rv && rm -rf awesome-terminal-fonts
