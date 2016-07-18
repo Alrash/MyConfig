96904e1ea7bdf5569b05c389370b7bd881be925c
alrash <kasukuikawai@gmail.com>
2016-07-18 21:30:25 +0800

    add get phpstorm themt

--- script/php.sh
+++ script/php.sh
@@ -7,3 +7,6 @@
 
 echo "" && echo “get php, phpstorm and nginx“
 yaourt -S php php-cgi php-fpm phpstorm nginx
+
+echo "" && echi "get phpstorm theme yka and save in home folder"
+wget -b http://www.phpstorm-themes.com/sites/default/files/yka_0.icl -P /home -O yka_0.icls
